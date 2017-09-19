1dd5beb100a58e68bdf069e6ed6b61416876b41e
Max Nokso <maxmac@Maxs-Mac-mini.local>
2017-09-19 15:44:46 +0300

    fix stuff

--- src/ios/GooglePlacePickerPlugin.h
+++ src/ios/GooglePlacePickerPlugin.h
@@ -1,6 +1,11 @@
 #import <Cordova/CDV.h>
 
 @interface GooglePlacePickerPlugin : CDVPlugin
+{
+    NSString* callbackId;
+}
+
+@property (copy) NSString* callbackId;
 
 - (void)pickPlace:(CDVInvokedUrlCommand*)command;
 
